00c94704edf3bf3f43d57aec5a36c216658673b7
Jared Stemen <jstemen@gmail.com>
2016-10-21 13:49:53 -0700

    adding solution 2

--- solution.go
+++ solution.go
@@ -1,5 +1,11 @@
 package roblox_challenge
 
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
 func Merger(slice1 []int32, slice2 []int32) ([]int32) {
 	i1 := 0
 	i2 := 0
@@ -28,5 +34,113 @@ func Merger(slice1 []int32, slice2 []int32) ([]int32) {
 
 	}
 	return acc
+}
+
+func overflowDetectingMulti(nums ...int32) (prod int32, err error) {
+
+	fmt.Printf("Multiplying #%v\n", nums)
+	prod = nums[0]
+	for i := 1; i < len(nums); i++ {
+		x := nums[i]
+
+		fmt.Printf("x:%d * prod:%d = %d\n", x, prod, x * prod)
+		if (x == -1 && prod == math.MinInt32) || (x == math.MinInt32 && prod == -1) {
+			err = errors.New(fmt.Sprintf("Overflow detected x: %d, prod: %d\n", x, prod))
+			return
+		}
+
+		/*
+		Assuming our arguments are currently already valid int32s:
+		1) multiplying by a 0 will always yield 0
+		2) multiplying by -1 will always yield a valid number expect for above case
+		3) multiplying by 1 will never change the product
+		*/
+		if (prod > 1 || prod < -1) &&  (x > 1 || x < -1) {
+			if prod > 0 {
+				fmt.Println("prod is greater than 0 ")
+				if x > (math.MaxInt32 / prod ) {
+					err = errors.New(fmt.Sprintf("Overflow detected x: %d, prod: %d\n", x, prod))
+					return
+				}
+				fmt.Printf("(math.MinInt32 / prod ) = %d \n", (math.MinInt32 / prod ))
+				if x < (math.MinInt32 / prod ) {
+					err = errors.New(fmt.Sprintf("Underflow detected x: %d, prod: %d\n", x, prod))
+					return
+				}
+			}else {
+				fmt.Println("prod is less than 0 ")
+				if x < (math.MaxInt32 / prod ) {
+					err = errors.New(fmt.Sprintf("Overflow detected x: %d, prod: %d\n", x, prod))
+					return
+				}
+				if x > (math.MinInt32 / prod ) {
+					err = errors.New(fmt.Sprintf("Underflow detected x: %d, prod: %d div: %d\n", x, prod, math.MinInt32 / prod))
+					return
+				}
+			}
+		}
+		prod *= x
+	}
+	return
+}
+
+func FindLargestProduct(slice1 []int32) (prod int32, err error) {
+
+	if len(slice1) == 0 {
+		return
+	}else if len(slice1) == 1 {
+		prod = slice1[0]
+		return
+	}else if len(slice1) == 2 {
+		prod = slice1[0] * slice1[1]
+		return
+	}
+
+	var pos1 int32 = math.MinInt32
+	var pos2 int32 = math.MinInt32
+	var pos3 int32 = math.MinInt32
+
+	var neg1 int32 = math.MaxInt32
+	var neg2 int32 = math.MaxInt32
+
+	for _, v := range slice1 {
+		if v > pos1 {
+			pos3 = pos2
+			pos2 = pos1
+			pos1 = v
+		}else if (v > pos2) {
+			pos3 = pos2
+			pos2 = v
+		}else if (v > pos3) {
+			pos3 = v
+		}
+
+		if v < neg1 {
+			neg2 = neg1
+			neg1 = v
+		}else if (v < neg2) {
+			neg2 = v
+		}
+
+	}
+
+	p1, err := overflowDetectingMulti(pos1, pos2, pos3)
+	if err != nil {
+		return
+	}
+
+	//Multiplying negative numbers first should prevent underflow
+	p2, err := overflowDetectingMulti(neg1, neg2, pos1)
+	if err != nil {
+		return
+	}
+
+	if p1 > p2 {
+		prod = p1
+	}else {
+		prod = p2
+	}
+
+	return
 }
 
--- solution_test.go
+++ solution_test.go
@@ -1,9 +1,9 @@
-package roblox_challenge_test
+package roblox_challenge
 
 import (
-	. "github.com/jstemen/roblox_challenge"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/ginkgo"
+	"math"
 )
 
 var _ = Describe("solution", func() {
@@ -34,6 +34,155 @@ var _ = Describe("solution", func() {
 			s2 := []int32{1, 3, 4, 5}
 			merged := Merger(s1, s2)
 			Expect(merged).To(Equal([]int32{0, 1, 2, 3, 3, 4, 5}))
+		})
+
+	})
+
+	Context("Multiply", func() {
+
+		It("Should return 0 when the slice is empty", func() {
+			s1 := make([]int32, 0)
+			product, err := FindLargestProduct(s1)
+			Expect(err).To(BeNil())
+			Expect(product).To(Equal(int32(0)))
+		})
+
+		It("Should return the number when this is only one", func() {
+			s1 := []int32{3}
+			product, err := FindLargestProduct(s1)
+			Expect(err).To(BeNil())
+			Expect(product).To(Equal(int32(3)))
+		})
+
+		It("Should return the product when only 2 numbers are given", func() {
+			s1 := []int32{3, 2}
+			product, err := FindLargestProduct(s1)
+			Expect(err).To(BeNil())
+			Expect(product).To(Equal(int32(6)))
+		})
+
+		It("Should work for all positives", func() {
+			s1 := []int32{10, 3, 10, 2, 12, 5 }
+			product, err := FindLargestProduct(s1)
+			Expect(err).To(BeNil())
+			Expect(product).To(Equal(int32(1200)))
+		})
+
+		It("Should work with the given example", func() {
+			s1 := []int32{10, 3, 0, -2, 12, 5, 1, 1, 4}
+			product, err := FindLargestProduct(s1)
+			Expect(err).To(BeNil())
+			Expect(product).To(Equal(int32(600)))
+		})
+
+		It("Should work for all negitives", func() {
+			s1 := []int32{-3, -1, -3, -10}
+			product, err := FindLargestProduct(s1)
+			Expect(err).To(BeNil())
+			Expect(product).To(Equal(int32(-9)))
+		})
+
+		It("Should return error when product greater than int32 max", func() {
+			s1 := []int32{2147483646, 214748364, 21483647}
+			_, err := FindLargestProduct(s1)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("Overflow detected"))
+		})
+		It("Should return error when product less than int32 min", func() {
+			s1 := []int32{1, -2147483648, 2147483647}
+			_, err := FindLargestProduct(s1)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("Underflow detected"))
+		})
+	})
+
+	Context("multi", func() {
+
+		//main logic test cases
+		It("Should error on intMax * intMax", func() {
+			_, err := overflowDetectingMulti(math.MaxInt32, math.MaxInt32)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("Overflow detected"))
+		})
+
+		It("Should error on intMax * initMin", func() {
+			_, err := overflowDetectingMulti(math.MinInt32, math.MaxInt32)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("Underflow detected"))
+		})
+
+		It("Should error on initMin * intMax", func() {
+			_, err := overflowDetectingMulti(math.MinInt32, math.MaxInt32)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("Underflow detected"))
+		})
+
+		It("Should error on initMin * initMin", func() {
+			_, err := overflowDetectingMulti(math.MinInt32, math.MinInt32)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("Overflow detected"))
+		})
+
+		// 2's complement test cases
+
+		It("Should error on initMin * -1", func() {
+			_, err := overflowDetectingMulti(math.MinInt32, -1)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("Overflow detected"))
+		})
+
+		It("Should error on -1 * initMin", func() {
+			_, err := overflowDetectingMulti(-1, math.MinInt32)
+			Expect(err).ToNot(BeNil())
+			Expect(err.Error()).To(MatchRegexp("Overflow detected"))
+		})
+
+		//-1
+		It("Should not error on -1 * -1", func() {
+			_, err := overflowDetectingMulti(-1, -1)
+			Expect(err).To(BeNil())
+		})
+
+		It("Should not error on -1 * 0", func() {
+			_, err := overflowDetectingMulti(-1, 0)
+			Expect(err).To(BeNil())
+		})
+
+		It("Should not error on -1 * -1", func() {
+			_, err := overflowDetectingMulti(-1, 0)
+			Expect(err).To(BeNil())
+		})
+		//0
+		It("Should not error on 0 * -1", func() {
+			_, err := overflowDetectingMulti(0, -1)
+			Expect(err).To(BeNil())
+		})
+
+		It("Should not error on 0 * 0", func() {
+			_, err := overflowDetectingMulti(0, 0)
+			Expect(err).To(BeNil())
+		})
+
+		It("Should not error on 0 * 1", func() {
+			_, err := overflowDetectingMulti(0, 1)
+			Expect(err).To(BeNil())
+		})
+
+		//1
+
+		It("Should not error on 1 * -1", func() {
+			_, err := overflowDetectingMulti(1, -1)
+			Expect(err).To(BeNil())
+		})
+
+		It("Should not error on 1 * 0", func() {
+			_, err := overflowDetectingMulti(1, 0)
+			Expect(err).To(BeNil())
+		})
+
+		It("Should not error on 1 * 1", func() {
+			_, err := overflowDetectingMulti(1, 1)
+			Expect(err).To(BeNil())
 		})
 
 	})
